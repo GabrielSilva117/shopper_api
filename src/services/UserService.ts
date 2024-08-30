4770dfabbe97edb3fc7185ffa8ac7e27ed509491
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-30 20:10:12 -0300

    feat: Adds a user entity related service

---
+++ src/services/UserService.ts
@@ -0,0 +1,22 @@
+import {AppDataSource} from "../data-source";
+import {Users} from "../entities/Users";
+
+class UserService {
+    createUser (code: number) {
+        try {
+            return AppDataSource.createQueryBuilder()
+                .insert()
+                .into(Users)
+                .values({
+                    code,
+                    name: 'user'+code,
+                })
+                .orIgnore()
+                .execute()
+        } catch (e) {
+            console.error(e);
+        }
+    };
+}
+
+export default new UserService();
