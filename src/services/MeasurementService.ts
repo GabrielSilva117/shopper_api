ae9397d2c7511c1bf8463cb696d265ff7441c2c3
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-30 20:15:27 -0300

    feat: Adds a Measurement Entity related service

---
+++ src/services/MeasurementService.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from "express";
+import {AppDataSource} from "../data-source";
+import {Measurements} from "../entities/Measuraments";
+import GeminiService from "./GeminiService";
+import {MeasurementTypeEnum} from "../models/MeasurementTypeEnum";
+import UserService from "./UserService";
+
+export class MeasurementService {
+}
+
+export default new MeasurementService();
