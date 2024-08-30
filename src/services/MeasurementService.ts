cad58f6846e5bc2ec579e8117c07f1fb7057a86e
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-30 20:18:03 -0300

    feat: Adds a method to count the rows returned from the query
    Checks if there is any measurement in the datatime + customeCode + measureType specified by the params

--- src/services/MeasurementService.ts
+++ src/services/MeasurementService.ts
@@ -6,6 +6,17 @@ import {MeasurementTypeEnum} from "../models/MeasurementTypeEnum";
 import UserService from "./UserService";
 
 export class MeasurementService {
+    static async searchMeasurement(dateTime: Date, customerCode: string, measureType: number) {
+        const [measurements, count] = await AppDataSource.getRepository(Measurements)
+            .createQueryBuilder('measurement')
+            .leftJoinAndSelect('measurement.user', 'user')
+            .where('user.code = :customerCode', { customerCode })
+            .andWhere('measurement.measurementDate = :dateTime', { dateTime })
+            .andWhere('measurement.type = :measureType', { measureType })
+            .getManyAndCount();
+
+        return count > 0;
+    }
 }
 
 export default new MeasurementService();
